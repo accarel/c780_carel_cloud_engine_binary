222a241a5ba2e8cad1eb53d145d3cbae28e52e45
valentina.cellini <valentina.cellini@34324a06-f003-4e88-b7c4-8fccacb9a23c>
2019-10-01 08:39:06 +0000

    FS: functions renamed

--- Projects/Src/File_System_CAREL.c
+++ Projects/Src/File_System_CAREL.c
@@ -186,25 +186,25 @@ C_RES File_System_DefaultConfig_Load(void)
 }
 
 /**
- * @brief File_System_Config_Get_RS485_baudrate
+ * @brief Get_RS485_baudrate
  *        Gets RS485 baud rate from configuration file
  * 
  * @param void
  * @return C_UINT32 contaning baud rate
  */
-C_UINT32 File_System_Config_Get_RS485_baudrate(void)
+C_UINT32 Get_RS485_baudrate(void)
 {  /* TO BE implemented */
 	return 1;
 }
 
 /**
- * @brief File_System_Config_Get_Device
+ * @brief Get_Device_Address
  *        Gets RS485 slave device address from configuration file
  * 
  * @param void
  * @return C_UINT16 contaning device address 
  */
-C_UINT16 File_System_Config_Get_Device(void)
+C_UINT16 Get_Device_Address(void)
 {  /* TO BE implemented */
 	return 1;
 }
@@ -301,13 +301,13 @@ C_RES File_System_Model_Store(void)
 }
 
 /**
- * @brief File_System_Model_Get_Guid
+ * @brief Get_Guid
  *        Gets guid from model file
  * 
  * @param void
  * @return C_SUCCESS or C_FAIL
  */
-C_RES File_System_Model_Get_Guid(C_BYTE* Guid)
+C_RES Get_Guid(C_BYTE* Guid)
 {  /* TO BE implemented */
 	
 	return 1;
