6665b9ad520de994e350d275b482a4f4e9aab54c
valentina.cellini <valentina.cellini@34324a06-f003-4e88-b7c4-8fccacb9a23c>
2019-10-01 08:40:04 +0000

    RTOS: added wrapper for OS initialization

---
+++ Projects/Inc/RTOS_IS.h
@@ -0,0 +1,38 @@
+/**
+ * @file RTOS_IS.h
+ * @author carel
+ * @date 1 Oct 2019
+ * @brief  functions implementations specific related to the managment of the 
+ *         operating system.
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __RTOS_IS_H
+#define __RTOS_IS_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "data_types_CAREL.h"   
+   
+   
+/* Exported types ------------------------------------------------------------*/ 
+
+
+
+
+/* Exported constants --------------------------------------------------------*/
+
+
+
+/* Function prototypes -------------------------------------------------------*/
+C_RES RTOS_Init(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Projects/Src/RTOS_IS.c
@@ -0,0 +1,46 @@
+/**
+ * @file RTOS_IS.c
+ * @author carel
+ * @date 1 Oct 2019
+ * @brief  functions implementations specific related to the managment of the
+ *         operating system.
+ */
+
+
+
+
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "RTOS_IS.h"   
+   
+   
+
+/* Functions Implementation --------------------------------------------------*/
+
+/**
+ * @brief RTOS_Init
+ *        Initialize the operating system
+ * 
+ * @param none
+ * @return C_SUCCESS or C_FAIL
+ */
+C_RES RTOS_Init(void)
+{  /* TO BE implemented */
+   
+   /* call the routine to initialize the file system */
+  
+	
+  if (1)
+  {
+      return C_SUCCESS;
+  }
+  else
+  {
+	  return C_FAIL;
+  }
+	    	
+	
+}
+
+
