2dd69e4caabac383de812590a01a448f9893ada1
valentina.cellini <valentina.cellini@34324a06-f003-4e88-b7c4-8fccacb9a23c>
2019-10-01 08:40:43 +0000

    Button: added wrapper for button management

---
+++ Projects/Src/Button_IS.c
@@ -0,0 +1,26 @@
+/**
+ * @file Button_IS.c
+ * @author carel
+ * @date 1 Oct 2019
+ * @brief  functions implementations specific related to the managment of the
+ *         button.
+ */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "Button_IS.h"
+
+/* Functions implementations -------------------------------------------------------*/
+/**
+ * @brief Button_Init
+ *        Initializes button gpio 
+ *           
+ * @param void
+ * @return none
+ */
+void Button_Init(void)
+{ /* TO BE implemented */
+	
+}
+
+
