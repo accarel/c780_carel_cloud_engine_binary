2db357562dc2ca0f8a252c9ae9732eb3a6b3e822
valentina.cellini <valentina.cellini@34324a06-f003-4e88-b7c4-8fccacb9a23c>
2019-10-01 08:41:15 +0000

    Leds: added wrapper for leds management

---
+++ Projects/Inc/Leds_IS.h
@@ -0,0 +1,39 @@
+/**
+ * @file Leds_IS.h
+ * @author carel
+ * @date 1 Oct 2019
+ * @brief  functions implementations specific related to the managment of the
+           button.
+ */
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __LEDS_IS_H
+#define __LEDS_IS_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "data_types_CAREL.h"      
+   
+   
+/* Exported types ------------------------------------------------------------*/ 
+
+
+
+
+/* Exported constants --------------------------------------------------------*/
+
+
+
+/* Function prototypes -------------------------------------------------------*/
+
+void Leds_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Projects/Src/Leds_IS.c
@@ -0,0 +1,26 @@
+/**
+ * @file Leds_IS.c
+ * @author carel
+ * @date 1 Oct 2019
+ * @brief  functions implementations specific related to the managment of 
+ *         leds.
+ */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "Leds_IS.h"
+
+/* Functions implementations -------------------------------------------------------*/
+/**
+ * @brief Leds_Init
+ *        Initializes leds gpio 
+ *           
+ * @param void
+ * @return none
+ */
+void Leds_Init(void)
+{ /* TO BE implemented */
+	
+}
+
+
