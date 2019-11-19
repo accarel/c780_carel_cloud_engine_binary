f3e0b4e2095fb5128418480ceb263548d0168c53
valentina.cellini <valentina.cellini@34324a06-f003-4e88-b7c4-8fccacb9a23c>
2019-11-19 15:22:47 +0000

    patches: add patch application for modbus command 17

--- Projects/get_esp-idf.sh
+++ Projects/get_esp-idf.sh
@@ -7,3 +7,6 @@ git submodule update --init --recursive
 # this will apply possible patches
 # patch modbus implementation for esp32
 patch components/freemodbus/common/esp_modbus_master.c ~/esp/GME_Binary/patches/0001-esp-modbus-master.patch
+# manage cmd 17
+patch components/freemodbus/modbus/functions/mbfuncother.c ~/esp/GME_Binary/patches/0002_manage_cmd17.patch
+patch components/freemodbus/modbus/include/mb_m.h ~/esp/GME_Binary/patches/0003_manage_cmd17.patch
