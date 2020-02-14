d2d8dd09f5f8f3015319ca464f85380398872643
alessandro.chiebao <alessandro.chiebao@34324a06-f003-4e88-b7c4-8fccacb9a23c>
2020-02-14 14:11:01 +0000

    this software generate two file (cfg_data.bin and cfg_data_usr.bin)

---
+++ Utility/SpiffsSoftwareGenerator/CustumCfgGenerator/CustumCfgGenerator/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace CustumCfgGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textMqttBroker = new System.Windows.Forms.TextBox();
+            this.textMqttPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textMqttPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textMqttUser = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textNtpPort = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textNtpAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textVersion = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBetaVersion = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button1.Location = new System.Drawing.Point(337, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 92);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Generator";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "mqtt broker";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textMqttBroker
+            // 
+            this.textMqttBroker.Location = new System.Drawing.Point(89, 222);
+            this.textMqttBroker.Name = "textMqttBroker";
+            this.textMqttBroker.Size = new System.Drawing.Size(186, 22);
+            this.textMqttBroker.TabIndex = 3;
+            this.textMqttBroker.TextChanged += new System.EventHandler(this.textMqttBroker_TextChanged);
+            // 
+            // textMqttPort
+            // 
+            this.textMqttPort.Location = new System.Drawing.Point(89, 309);
+            this.textMqttPort.Name = "textMqttPort";
+            this.textMqttPort.Size = new System.Drawing.Size(186, 22);
+            this.textMqttPort.TabIndex = 4;
+            this.textMqttPort.TextChanged += new System.EventHandler(this.textMqttPort_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(86, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "mqtt port";
+            // 
+            // textMqttPassword
+            // 
+            this.textMqttPassword.Location = new System.Drawing.Point(380, 309);
+            this.textMqttPassword.Name = "textMqttPassword";
+            this.textMqttPassword.Size = new System.Drawing.Size(183, 22);
+            this.textMqttPassword.TabIndex = 6;
+            this.textMqttPassword.TextChanged += new System.EventHandler(this.textMqttPassword_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(377, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "mqtt password";
+            // 
+            // textMqttUser
+            // 
+            this.textMqttUser.Location = new System.Drawing.Point(380, 222);
+            this.textMqttUser.Name = "textMqttUser";
+            this.textMqttUser.Size = new System.Drawing.Size(183, 22);
+            this.textMqttUser.TabIndex = 5;
+            this.textMqttUser.TextChanged += new System.EventHandler(this.textMqttUser_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(377, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "mqtt user";
+            // 
+            // textNtpPort
+            // 
+            this.textNtpPort.Location = new System.Drawing.Point(656, 309);
+            this.textNtpPort.Name = "textNtpPort";
+            this.textNtpPort.Size = new System.Drawing.Size(184, 22);
+            this.textNtpPort.TabIndex = 8;
+            this.textNtpPort.TextChanged += new System.EventHandler(this.textNtpPort_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(652, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "ntp port";
+            // 
+            // textNtpAddress
+            // 
+            this.textNtpAddress.Location = new System.Drawing.Point(656, 222);
+            this.textNtpAddress.Name = "textNtpAddress";
+            this.textNtpAddress.Size = new System.Drawing.Size(184, 22);
+            this.textNtpAddress.TabIndex = 7;
+            this.textNtpAddress.TextChanged += new System.EventHandler(this.textNtpAddress_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(652, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "ntp address";
+            // 
+            // textVersion
+            // 
+            this.textVersion.Location = new System.Drawing.Point(89, 129);
+            this.textVersion.Name = "textVersion";
+            this.textVersion.Size = new System.Drawing.Size(88, 22);
+            this.textVersion.TabIndex = 1;
+            this.textVersion.TextChanged += new System.EventHandler(this.textVersion_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(86, 94);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(179, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "versione config. default";
+            // 
+            // textBetaVersion
+            // 
+            this.textBetaVersion.Location = new System.Drawing.Point(207, 129);
+            this.textBetaVersion.Name = "textBetaVersion";
+            this.textBetaVersion.Size = new System.Drawing.Size(88, 22);
+            this.textBetaVersion.TabIndex = 2;
+            this.textBetaVersion.TextChanged += new System.EventHandler(this.textBetaVersion_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(183, 129);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "B";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 564);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBetaVersion);
+            this.Controls.Add(this.textVersion);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textNtpPort);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textNtpAddress);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textMqttPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textMqttUser);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textMqttPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textMqttBroker);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GME Default Parameter Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textMqttBroker;
+        private System.Windows.Forms.TextBox textMqttPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textMqttPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textMqttUser;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textNtpPort;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textNtpAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textVersion;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBetaVersion;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Utility/SpiffsSoftwareGenerator/CustumCfgGenerator/CustumCfgGenerator/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+
+namespace CustumCfgGenerator
+{
+    public partial class Form1 : Form
+    {
+        public const int NUM_OF_CFG_VERSION = 2;
+        public const int NUM_OF_BROKER      = 60;
+        public const int NUM_OF_PORT        = 30;
+        public const int NUM_OF_PSSW        = 34;
+        public const int NUM_OF_USER        = 34;
+        public const int NUM_OF_NTPSERVER   = 30;
+        public const int NUM_OF_NTPPORT     = 6;
+
+        byte[] cfg_version = new byte[NUM_OF_CFG_VERSION];
+        byte[] mqtt_broker = null;
+        byte[] mqtt_port = null;
+        byte[] mqtt_pssw = null;
+        byte[] mqtt_user = null;
+        byte[] ntp_server = null;
+        byte[] ntp_port = null;
+        
+  
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cfg_data     = null;
+            string cfg_data_usr = null; 
+
+
+            // genera file .bin per esp32
+            // controllo il contenuto delle textbox
+            try
+            {   
+                if (mqtt_broker.Length != null && mqtt_port.Length != null && mqtt_pssw.Length != null && mqtt_user.Length != null && ntp_server.Length != null && ntp_port.Length != null)
+                {
+                    string exeFolder = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+
+                    // "C:\\Users\\alessandro.chiebao\\Desktop\\CHIEBAO_PROGETTI\\GATEWAY_MIDDLE_END\\Software\\CfgCreator\\
+
+                    cfg_data = exeFolder + "\\cfg_data.bin";
+                    File.Delete(cfg_data);
+                    cfg_data_usr = exeFolder + "\\cfg_data_usr.bin";
+                    File.Delete(cfg_data_usr);
+
+                    using (FileStream stream = new FileStream(cfg_data, FileMode.Create))
+                    {
+                        using (BinaryWriter writer = new BinaryWriter(stream))
+                        {
+                            writer.Write(cfg_version);
+                            writer.Write(mqtt_broker);
+                            writer.Write(mqtt_port);
+
+                            writer.Write(mqtt_pssw);
+                            writer.Write(mqtt_user);
+
+                            writer.Write(ntp_server);
+                            writer.Write(ntp_port);
+                            writer.Close();
+
+
+                            var source = @cfg_data;
+                            var destination = @cfg_data_usr;
+                            try
+                            {
+                                File.Copy(source, destination);
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Rimuovere i file precedenti o rinominarli");
+                            }
+
+                            button1.BackColor = Color.Green;
+
+                            mqtt_broker = null;
+                            mqtt_port = null;
+                            mqtt_pssw = null;
+                            mqtt_user = null;
+                            ntp_server = null;
+                            ntp_port = null;                        
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Immettere tutti i campi");
+            }
+        }
+
+        private void textMqttBroker_TextChanged(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Gray;
+
+            mqtt_broker = new byte[NUM_OF_BROKER];
+
+            string newBroker = textMqttBroker.Text;
+
+            byte[] tmp = Encoding.ASCII.GetBytes(newBroker);
+
+            //int startAt = mqtt_broker.Length - tmp.Length;
+            Array.Copy(tmp, 0, mqtt_broker, 0, tmp.Length);
+        }
+
+        private void textMqttPort_TextChanged(object sender, EventArgs e)
+        {
+            mqtt_port = new byte[NUM_OF_PORT];
+            
+            button1.BackColor = Color.Gray;
+
+            string newPort = textMqttPort.Text;
+
+            byte[] tmp = Encoding.ASCII.GetBytes(newPort);
+             
+            Array.Copy(tmp, 0, mqtt_port, 0, tmp.Length);
+        }
+
+        private void textMqttPassword_TextChanged(object sender, EventArgs e)
+        {
+            mqtt_pssw = new byte[NUM_OF_PSSW];
+
+            button1.BackColor = Color.Gray;
+
+            string newPass = textMqttPassword.Text;
+
+            byte[] tmp = Encoding.ASCII.GetBytes(newPass);
+
+            Array.Copy(tmp, 0, mqtt_pssw, 0, tmp.Length);  
+        }
+        private void textMqttUser_TextChanged(object sender, EventArgs e)
+        {
+            mqtt_user = new byte[NUM_OF_USER];
+
+            button1.BackColor = Color.Gray;
+
+            string newUsr = textMqttUser.Text;
+
+            byte[] tmp = Encoding.ASCII.GetBytes(newUsr);
+
+            Array.Copy(tmp, 0, mqtt_user, 0, tmp.Length);
+        }
+
+        private void textNtpAddress_TextChanged(object sender, EventArgs e)
+        {
+            ntp_server = new byte[NUM_OF_NTPSERVER];
+            
+            button1.BackColor = Color.Gray;
+
+            string newNtpServer = textNtpAddress.Text;
+
+            byte[] tmp = Encoding.ASCII.GetBytes(newNtpServer);
+
+            Array.Copy(tmp, 0, ntp_server, 0, tmp.Length);
+        }
+
+        private void textNtpPort_TextChanged(object sender, EventArgs e)
+        {
+            ntp_port = new byte[NUM_OF_NTPPORT];
+
+            button1.BackColor = Color.Gray;
+
+            string newNtpPort = textNtpPort.Text;
+
+            byte[] tmp = Encoding.ASCII.GetBytes(newNtpPort);
+
+            Array.Copy(tmp, 0, ntp_port, 0, tmp.Length);
+        }
+
+        private void textVersion_TextChanged(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Gray;
+
+            string newVersion = textVersion.Text;
+
+            try
+            {
+                byte Majnumber = Convert.ToByte(newVersion);
+
+                if (Enumerable.Range(1, 256).Contains(Majnumber))
+                {
+                    cfg_version[0] = Majnumber;
+                }
+                else
+                {
+                    MessageBox.Show("Valore errato (range valido da 1 - 256)");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Valore errato (range valido da 1 - 256)");
+            }
+
+        }
+
+        private void textBetaVersion_TextChanged(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Gray;
+
+            string newVersion = textBetaVersion.Text;
+        
+            try
+            {
+                byte Minnumber = Convert.ToByte(newVersion);
+
+                if (Enumerable.Range(0, 99).Contains(Minnumber))
+                {
+                   cfg_version[1] = Minnumber;
+                }
+                else
+                {
+                    MessageBox.Show("Valore errato (range valido da 0 - 99)");
+                }
+            }
+            catch
+            {
+                    MessageBox.Show("Valore errato (range valido da 0 - 99)");        
+            }
+        }
+    }
+}
