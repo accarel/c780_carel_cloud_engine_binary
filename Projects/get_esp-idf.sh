1089f2f562f09dcef750c268dd757e6048eeb2b1
valentina.cellini <valentina.cellini@34324a06-f003-4e88-b7c4-8fccacb9a23c>
2020-02-18 10:43:14 +0000

    esp-idf: update esp-idf to official release v4.0 Download whole package from scratch or alternatively, when updating from a previous version, follow instructions @ https://docs.espressif.com/projects/esp-idf/en/latest/versions.html#updating
    cd esp/esp-idf
    git fetch
    git checkout v4.0
    git submodule update --init --recursive

--- Projects/get_esp-idf.sh
+++ Projects/get_esp-idf.sh
@@ -1,6 +1,6 @@
 cd ~/esp
 # this will create a folder esp-idf with version 4.0-beta2
-git clone -b v4.0-beta2  https://github.com/espressif/esp-idf.git
+git clone -b v4.0  https://github.com/espressif/esp-idf.git
 cd ~/esp/esp-idf
 git submodule update --init --recursive
 
