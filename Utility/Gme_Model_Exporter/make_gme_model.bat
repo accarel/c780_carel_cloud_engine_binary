cc2ae7cfc1acefc4ff0800d628dafd7b97f2d28d
alessandro.bilato <alessandro.bilato@34324a06-f003-4e88-b7c4-8fccacb9a23c>
2020-04-20 08:50:19 +0000

    1st ver of the program from IoT to make a model.bin file, included an example and a batch file to do

---
+++ Utility/Gme_Model_Exporter/make_gme_model.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Java\jdk-12.0.1\bin\java.exe" -jar gme-model-exporter.jar --xml-supervisor gme -m c:\hwfwdept_proj\c780_carel_cloud_engine_binary\Utility\Gme_Model_Exporter\cpco_app_demo.xml
+pause
