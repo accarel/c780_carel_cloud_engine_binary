62a7842d656f135c7e7e86650afccd4b444de5e0
valentina.cellini <valentina.cellini@34324a06-f003-4e88-b7c4-8fccacb9a23c>
2020-04-23 13:22:51 +0000

    CARE_GLOBAL_DEF: remove unused #defines

--- Projects/GME_Binary/main/CAREL_GLOBAL_DEF.h
+++ Projects/GME_Binary/main/CAREL_GLOBAL_DEF.h
@@ -102,17 +102,6 @@
 #define GW_PARTNUMBER "GTW000M2G0"
 #define GW_HW_REV  256 //0x100
 #define GW_FW_REV  256 //0x100
-#define GW_CLOUDIFACE_TYPE  "wifi"
-#define GW_CLOUDIFACE_NAME  "WIFI-1"
-#define GW_CLOUDIFACE_SUBTYPE "2.4 GHz"       
-#define GW_CLOUDIFACE_MODE "client"
-
-#define GW_CLOUDIFACE_MAC_ADDRESS "3C:71:BF:F7:F4"
-
-/* ========================================================================== */
-/* File system related                                                        */
-/* ========================================================================== */
-
 
 /* ========================================================================== */
 /* OS related                                                                 */
@@ -124,27 +113,6 @@
 */
 
 #define SYSTEM_TIME_TICK	1
-
-/* ========================================================================== */
-/* Enum data types MQTT                                                        */
-/* ========================================================================== */
-#define MQTT_BOOL		0	 
-#define MQTT_INT16		1
-#define MQTT_UINT16 	2
-#define MQTT_INT32 	  	3
-#define MQTT_UINT32   	4
-#define MQTT_FLOAT	  	5
-#define MQTT_VARIANT	6
-
-
-/* ========================================================================== */
-/* Non volatile memory related  NVM#                                          */
-/* ========================================================================== */
-#define MQTT_CURRENT_BROKER   (tbd)
-#define MQTT_CURRENT_PORT     (tbd)
-#define MQTT_CURRENT_USER     (tbd)
-#define MQTT_CURRENT_PWD      (tbd)
-
 
 /* ========================================================================== */
 /* Cloud related                                                              */
@@ -159,43 +127,6 @@
 
 #define NTP_DEFAULT_SERVER  "pool.ntp.org"
 #define NTP_DEFAULT_PORT  	123
-/* ========================================================================== */
-/* POLLING ENGINE RELATED                                                     */
-/* ========================================================================== */
-
-//polling time in seconds
-#define GW_HIGH_POLLING_TIME 300
-#define GW_LOW_POLLING_TIME 600
-
-
-/* ========================================================================== */
-/* ALARMS ENGINE RELATED                                                      */
-/* ========================================================================== */
-#define ALARM_TYPE_NOT_VALID 0
-#define ALARM_TYPE_VARIABLE  1
-#define ALARM_TYPE_DEVICE    2
-
-
-/* ========================================================================== */
-/* GW MQTT command related                                                    */
-/* ========================================================================== */
-//command (res)ponse value
-#define MQTT_CMD_INVALID -1
-#define MQTT_CMD_OK       0
-#define MQTT_CMD_ERROR    1
-
-#define PROTOCOL_ID_NOT_VALID  0
-#define PROTOCOL_ID_MODBUS     1
-#define PROTOCOL_ID_MODBUS_TCP 2
-#define PROTOCOL_ID_BACNET     3
-
-
-/* ========================================================================== */
-/* return values / Error values                                               */
-/* ========================================================================== */
-#define MQTT_CMD_RETURN_SUCCESS 0
-#define MQTT_CMD_RETURN_FAIL    1
-
 
 #endif
 
