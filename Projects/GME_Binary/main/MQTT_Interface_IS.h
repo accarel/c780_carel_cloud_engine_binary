fe4f25a3aed643dd86bcc33ee3955bc22778b750
valentina.cellini <valentina.cellini@34324a06-f003-4e88-b7c4-8fccacb9a23c>
2020-07-14 07:47:19 +0000

    mqtt: fix prototypes according to commit 721

--- Projects/GME_Binary/main/MQTT_Interface_IS.h
+++ Projects/GME_Binary/main/MQTT_Interface_IS.h
@@ -44,9 +44,9 @@ typedef esp_mqtt_client_handle_t mqtt_client_handle_t;
 
 
 /* Function prototypes -------------------------------------------------------*/
-C_RES mqtt_client_subscribe(C_SCHAR *topic, C_INT16 qos);
+C_INT32 mqtt_client_subscribe(C_SCHAR *topic, C_INT16 qos);
 C_RES mqtt_client_unsubscribe(C_SCHAR *topic);
-C_RES mqtt_client_publish(C_SCHAR *topic, C_SBYTE *data, C_INT16 len, C_INT16 qos, C_INT16 retain);
+C_INT32 mqtt_client_publish(C_SCHAR *topic, C_SBYTE *data, C_INT16 len, C_INT16 qos, C_INT16 retain);
 C_RES mqtt_client_start(void);
 C_RES mqtt_client_stop(void);
 void* mqtt_client_init(mqtt_config_t* mqtt_cfg_nvm);
