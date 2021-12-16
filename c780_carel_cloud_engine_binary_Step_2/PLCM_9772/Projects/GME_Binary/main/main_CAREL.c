245e60d7723790dd36d6ae0ce2bb0bb4829fe104
accarel <88131114+accarel@users.noreply.github.com>
2021-12-16 14:24:56 +0100

    removed all comment...now please merge with master

--- c780_carel_cloud_engine_binary_Step_2/PLCM_9772/Projects/GME_Binary/main/main_CAREL.c
+++ c780_carel_cloud_engine_binary_Step_2/PLCM_9772/Projects/GME_Binary/main/main_CAREL.c
@@ -302,9 +302,9 @@ void Carel_Main_Task(void)
         		NVM__ReadU8Value(SET_LINE_CONFIG_NVM, &line_config_status);
         		NVM__ReadU8Value(SET_DEVS_CONFIG_NVM, &devs_config_status);
 
-				if(	CONFIGURED == gw_config_status &&			//  restore the comment
-					CONFIGURED == line_config_status &&			//  restore the comment 
-					CONFIGURED == devs_config_status)			//  restore the comment 
+				if(	CONFIGURED == gw_config_status &&			//  
+					CONFIGURED == line_config_status &&			//  
+					CONFIGURED == devs_config_status)			// 
 				{
 					P_COV_LN;
 					sm = GME_REBOOT;
