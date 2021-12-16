f4c9ee22b33c5663a8ec58def76457992bd42e60
accarel <88131114+accarel@users.noreply.github.com>
2021-12-16 15:28:40 +0100

    added tag comment

--- c780_carel_cloud_engine_binary_Step_2/PLCM_9772/Projects/GME_Binary/main/main_CAREL.c
+++ c780_carel_cloud_engine_binary_Step_2/PLCM_9772/Projects/GME_Binary/main/main_CAREL.c
@@ -302,9 +302,9 @@ void Carel_Main_Task(void)
         		NVM__ReadU8Value(SET_LINE_CONFIG_NVM, &line_config_status);
         		NVM__ReadU8Value(SET_DEVS_CONFIG_NVM, &devs_config_status);
 
-				if(	CONFIGURED == gw_config_status &&			//  only V106
-					CONFIGURED == line_config_status &&			//  only V106
-					CONFIGURED == devs_config_status)			//  only V106
+				if(	CONFIGURED == gw_config_status &&			//  only V106 tag
+					CONFIGURED == line_config_status &&			//  only V106 tag
+					CONFIGURED == devs_config_status)			//  only V106 tag
 				{
 					P_COV_LN;
 					sm = GME_REBOOT;
