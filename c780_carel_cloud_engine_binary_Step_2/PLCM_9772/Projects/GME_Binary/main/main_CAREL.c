b3171931b09eb82bb859f8e286ff314c5f04bb4b
accarel <88131114+accarel@users.noreply.github.com>
2021-12-17 14:17:52 +0100

    commit commento master

--- c780_carel_cloud_engine_binary_Step_2/PLCM_9772/Projects/GME_Binary/main/main_CAREL.c
+++ c780_carel_cloud_engine_binary_Step_2/PLCM_9772/Projects/GME_Binary/main/main_CAREL.c
@@ -302,9 +302,9 @@ void Carel_Main_Task(void)
         		NVM__ReadU8Value(SET_LINE_CONFIG_NVM, &line_config_status);
         		NVM__ReadU8Value(SET_DEVS_CONFIG_NVM, &devs_config_status);
                 // pippo   
-				if(	CONFIGURED == gw_config_status &&			//  only V106 tag
-					CONFIGURED == line_config_status &&			//  only V106 tag
-					CONFIGURED == devs_config_status)			//  only V106 tag
+				if(	CONFIGURED == gw_config_status &&			//  master 
+					CONFIGURED == line_config_status &&			//  master
+					CONFIGURED == devs_config_status)			//  master
 				{
 					P_COV_LN;
 					sm = GME_REBOOT;
